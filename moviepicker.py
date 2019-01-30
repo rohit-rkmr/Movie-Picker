3095873447f2418e2e31f81900faf7ba1c9284c4
Rohit Ravikumar <42780825+rohit-rkmr@users.noreply.github.com>
2019-01-30 21:02:52 +0530

    Update moviepicker.py

--- moviepicker.py
+++ moviepicker.py
@@ -5,6 +5,15 @@ top250 = obj.get_top250_movies()
 c="y"
 while c=="y" or c=="Y":
  num=randint(0,250)
- print(top250[num])
+ movie=str(top250[num])
+ print(movie)
+ result = obj.search_movie(movie)
+ movdat = result[0]
+ obj.update(movdat)
+ print("Rating : ",movdat['rating'])
+ print("Genre : ",movdat['genre'])
+ for director in movdat['directors']:
+    print("Director : ",director['name'])
+ print("Runtime : ",movdat['runtime'],"min")
  c=input("Want to see something else(y/n)")
 
